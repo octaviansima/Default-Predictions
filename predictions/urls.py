b0d295310dcc4d1d21c956a692dcf859e6f2ec6b
Octavian <octavian.sima@berkeley.edu>
2018-07-14 18:08:09 -0400

    connected views

--- predictions/urls.py
+++ predictions/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('submit/', views.submit, name='submit'),
+    path('result/', views.result, name='result')
 ]
--- predictions/views.py
+++ predictions/views.py
@@ -1,4 +1,23 @@
+from django.contrib.messages import error
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from predictions.forms import FeaturesForm
+from predictions.engine import *
 
 def index(request):
     return render(request, 'predictions/index.html')
+
+def submit(request):
+    if request.method == 'POST':
+        form = FeaturesForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            print(cd)
+            request.session['prediction_result'] = "Will Default"
+            return HttpResponseRedirect('/result/')
+        else:
+            error(request, "Please fill in all fields.")
+            return HttpResponseRedirect('/')
+
+def result(request):
+    return render(request, 'predictions/result.html', {"prediction": request.session['prediction_result']})
